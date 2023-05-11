18513b1967a2a3b2bc0b83e274fb779647a714aa
Lorde <rioaformyking@gmail.com>
2023-05-11 17:18:08 +0800

    feat: init

---
+++ src/api/sys/model/userModel.ts
@@ -0,0 +1,55 @@
+/**
+ * @description: Login interface parameters
+ */
+export interface LoginParams {
+  username: string
+  password: string
+}
+
+/**
+ * @description: Register interface parameters
+ */
+export interface RegisterParams {
+  username: string
+  password: string
+  confirmPassword: string
+}
+
+export interface RoleInfo {
+  roleName: string
+  value: string
+}
+
+/**
+ * @description: Login interface return value
+ */
+export interface LoginResultModel {
+  userId: string | number
+  token?: string
+  role: RoleInfo
+  data: any
+}
+
+/**
+ * @description: Register interface return value
+ */
+export type RegisterResultModel = LoginResultModel & {
+  userId: string
+}
+
+/**
+ * @description: Get user information return value
+ */
+export interface GetUserInfoModel {
+  roles: RoleInfo[]
+  // 用户id
+  userId: string | number
+  // 用户名
+  username: string
+  // 真实名字
+  realName: string
+  // 头像
+  avatar: string
+  // 介绍
+  desc?: string
+}
---
+++ src/api/sys/user.ts
@@ -0,0 +1,73 @@
+import { defHttp } from '/@/utils/http/axios'
+import { LoginParams, LoginResultModel, GetUserInfoModel, RegisterParams } from './model/userModel'
+
+import { ErrorMessageMode, Result } from '/#/axios'
+
+enum Api {
+  Login = '/employee/login',
+  Logout = '/logout',
+  GetUserInfo = '/getUserInfo',
+  GetPermCode = '/getPermCode',
+  TestRetry = '/testRetry',
+  Register = '/employee/register',
+}
+
+/**
+ * @description: user login api
+ */
+export function loginApi(params: LoginParams, mode: ErrorMessageMode = 'modal') {
+  return defHttp.post<LoginResultModel>(
+    {
+      url: Api.Login,
+      params,
+    },
+    {
+      errorMessageMode: mode,
+      isTransformResponse: false,
+    },
+  )
+}
+
+/**
+ * @description: user register api
+ */
+export function registerApi(params: RegisterParams, mode: ErrorMessageMode = 'modal') {
+  return defHttp.post<Result>(
+    {
+      url: Api.Register,
+      params,
+    },
+    {
+      errorMessageMode: mode,
+      isTransformResponse: false,
+    },
+  )
+}
+
+/**
+ * @description: getUserInfo
+ */
+export function getUserInfo() {
+  return defHttp.get<GetUserInfoModel>({ url: Api.GetUserInfo }, { errorMessageMode: 'none' })
+}
+
+export function getPermCode() {
+  return defHttp.get<string[]>({ url: Api.GetPermCode })
+}
+
+export function doLogout() {
+  return defHttp.get({ url: Api.Logout })
+}
+
+export function testRetry() {
+  return defHttp.get(
+    { url: Api.TestRetry },
+    {
+      retryRequest: {
+        isOpenRetry: true,
+        count: 5,
+        waitTime: 1000,
+      },
+    },
+  )
+}
